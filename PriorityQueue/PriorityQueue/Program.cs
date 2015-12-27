bb3176e21a84a9e7f9d9d0bdae30476bdb6e0cb6
jonHuffman <jon@jonhuffman.com>
2015-12-27 16:27:08 -0500

    Updated README to reflect purpose of repo; Updated gitignore to ignore the bin folder; Created a console project for the creation of a PriorityQueue

---
+++ PriorityQueue/PriorityQueue/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace PriorityQueue
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+
+      //Just a stop to prevent the console application from automatically closing
+      Console.WriteLine("Press any key to close...");
+      Console.Read();
+    }
+  }
+}
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # AN-Dec2015
+
+This repository has been set up to track changes and streamline submission of ArenaNet's Mobile Test.
