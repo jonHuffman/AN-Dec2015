34139e10dab4642564a0bc45a493897323315f65
jonHuffman <jon@jonhuffman.com>
2015-12-29 23:32:49 -0500

    Set up parallaxing backgrounds so that it looks like we are moving. Ohhhh maaaannn!!!

---
+++ ThereBeDragons/Assets/Classes/Misc/UVScroll.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+
+namespace TBD
+{
+  [RequireComponent(typeof(Renderer))]
+  public class UVScroll : MonoBehaviour
+  {
+    [SerializeField]
+    private bool _scrollOnX = false;
+    [SerializeField]
+    private bool _scrollOnY = false;
+    [SerializeField]
+    private float _xScrollRate = 0.1f;
+    [SerializeField]
+    private float _yScrollRate = 0.1f;
+
+    private Material _mat;
+
+    void Awake()
+    {
+      _mat = GetComponent<Renderer>().material;
+    }
+
+    void Update()
+    {
+      Vector2 textureOffset = _mat.GetTextureOffset("_MainTex");
+
+      if (_scrollOnX == true)
+      {
+        textureOffset.x += _xScrollRate;
+
+        if (textureOffset.x > 1)
+        {
+          textureOffset.x -= 1;
+        }
+      }
+
+      if (_scrollOnY == true)
+      {
+        textureOffset.y += _yScrollRate;
+
+        if (textureOffset.y > 1)
+        {
+          textureOffset.y -= 1;
+        }
+      }
+
+      _mat.SetTextureOffset("_MainTex", textureOffset);
+    }
+  }
+}
